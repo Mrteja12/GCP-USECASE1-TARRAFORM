4cabe24552a861960e2da8c9800623494bb47bd6
Mr Tejaa <107294505+Mrteja12@users.noreply.github.com>
2023-05-07 16:34:32 +0530

    Add files via upload

---
+++ reference.tf
@@ -0,0 +1,6 @@
+https://www.fpgmaas.com/blog/deploying-a-flask-api-to-cloudrun
+onsole.cloud.google.com/networking/firewalls/list?project=project1-377909
+Initializing the backend...
+╷
+│ Error: Failed to get existing workspaces: querying Cloud Storage failed: googleapi: Error 403: teja.deloence@gmail.com does not have storage.objects.list access to the Google Cloud Storage bucket. Permission 'storage.objects.list' denied on resource (or it may not exist)., forbidden
+│
